2546b1edaebafd041f473db595b873f4c8631e9b
jtf <jonas@minet.uni-jena.de>
2012-10-18 19:40:56 +0200

    mergesort.h entfernen

--- examples/tools.h
+++ examples/tools.h
@@ -1,6 +1,13 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+//void funktion();
+
+struct buffer {
+  int * data;
+  int size;
+};
+
 int allocbuf(struct buffer * b, size_t size);
 void freebuf(struct buffer * b);
 void randbuf(struct buffer * b);
