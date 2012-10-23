a00847b7d527f68248e900591c6115aba813e1aa
jtf <jonas@minet.uni-jena.de>
2012-10-23 13:49:32 +0200

    inssort.c fertig stellen

--- examples/inssort.c
+++ examples/inssort.c
@@ -5,24 +5,24 @@
 
 void inssort(struct buffer *b)
 {
-  int * last;
   int * i;
   int * j;
-
   int item;
 
-  last = b->data + b->size-1;
-  i = b->data++; // mit dem Zweiten beginnen
-
-  while(i <= last++){
+  // mit dem Zweiten beginnen
+  i = b->data + 1; 
+  // bis zum Ende
+  while(i < b->data + b->size){
     item = *i;
-
+    //    printf("%d\t", item);
     j = i;
     //solange j nicht am Anfang und item kleiner als Vorgänger
-    while ((j > b->data) && (item < *(i--)))
+    while ((j > b->data) && (item < *(i-1)))
       {
-	*j = *(j--)	
+	// eins weiter kopieren und eins zurück gehen
+    	*j-- = *(j - 1);
       }
+    *j = item;
     i++;
   }
 }
@@ -30,22 +30,24 @@ void inssort(struct buffer *b)
 int main(int argc, char *argv[])
 {
   struct buffer b;
-  struct buffer tmpbuf;
 
-  int size = 23;
+  int size = 7;
 
-  if ( allocbuf(&b, size) && allocbuf(&tmpbuf, size) )
+  if ( allocbuf(&b, size) )
     {
       for (int i=0; i<size; i++)
-	b.data[i]=size-i;
+	b.data[i]=9-i;//size-i;
+
+      printf(" davor: ");
+      prtbhead(&b, b.size);
+      printf("\n");
 
       inssort(&b);
 
+      printf("\n danach: ");
       prtbhead(&b, b.size);
-
       printf("\n");
 
-      freebuf(&tmpbuf);
       freebuf(&b);
     }
   else
