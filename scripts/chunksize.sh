9a1d3b7e388f26a9a080e255a41119f11252cbe8
jtf <jonas@minet.uni-jena.de>
2012-12-14 17:29:05 +0100

    chunk-size-Iterations-Skript erweitern

--- scripts/chunksize.sh
+++ scripts/chunksize.sh
@@ -5,26 +5,39 @@ BIN=./bin/mpi-ms
 LOGFILE=./log
 
 # num of nodes
-NODES=2
-
+#NODES=8
 HOSTS=1
 
+
 # init sizes
-VOLUME=10000000
-CHUNK=11
+# VOLUME == FACTOR^n * CHUNK
+VOLUME=$((100 * 1000 * 1000))
+INITCHUNK=1000
+FACTOR=10
+
+
+for NODES in `seq 8 -2 2 `
+do
+
+# reset chunk size
+CHUNK=$INITCHUNK
+
 
 while [ ${CHUNK} -lt ${VOLUME} ]
 do
-    echo "Berechne: $CHUNK $VOLUME"
+    echo "Berechne: $NODES $CHUNK $VOLUME"
 
     # logging
     LOGSTRING=`hostname | tr -d "\n" ; echo -n " - "; date | tr -d "\n"`
 
-    mpirun -n $NODES $BIN -c $CHUNK -v $VOLUME >>"data/$HOSTS-$NODES-$VOLUME-$CHUNK.log"
-
+   mpirun -n $NODES $BIN -c $CHUNK -v $VOLUME >>"data/$HOSTS-$NODES-$VOLUME-$CHUNK.log"
 
     # logging
-    echo "$LOGSTRING: $HOSTS-$NODES-$VOLUME-$CHUNK">>$LOGFILE
+   echo "$LOGSTRING: $HOSTS-$NODES-$VOLUME-$CHUNK">>$LOGFILE
 
-    CHUNK=$((CHUNK*10))
+    CHUNK=$((CHUNK*FACTOR))
+# end of while chunk size
+done
+
+# end of for NODES
 done
